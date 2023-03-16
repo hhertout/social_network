fa99558f32b7ec12d8a1b66eb02b611885295ba1
hhertout <hugues.hertout@gmail.com>
2023-03-16 23:10:19 +0100

    Light fix

--- server/src/Schema/Post.schema.ts
+++ server/src/Schema/Post.schema.ts
@@ -3,7 +3,7 @@ import {Schema, model} from "mongoose";
 
 export const PostSchema = new Schema<IPost>({
     content: {type: String, required: true},
-    createdAt: {type: Date, default: () => Date.now()},
+    createdAt: {type: Date, immutable: true, default: () => Date.now()},
     updatedAt: {type: Date, default: Date.now},
 })
 
--- server/src/Schema/User.schema.ts
+++ server/src/Schema/User.schema.ts
@@ -3,7 +3,7 @@ import {IUser} from "../Types/User";
 import {PostSchema} from "./Post.schema";
 
 export const UserSchema = new Schema<IUser>({
-    createdAt: {type: Date, default: () => Date.now()},
+    createdAt: {type: Date, immutable: true, default: () => Date.now()},
     updatedAt: {type: Date, default: () => Date.now()},
     email: {type: String, required: true, unique: true},
     username: {type: String, required: true},
