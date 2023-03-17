7ac3b3fc9e19b45d3c631f763905ab9884897867
hhertout <hugues.hertout@gmail.com>
2023-03-17 12:06:01 +0100

    ENV Improvement

--- server/src/Config/database.config.ts
+++ server/src/Config/database.config.ts
@@ -1,5 +1,5 @@
 import * as mongoose from "mongoose";
 
 export const dbConnect = async () => {
-    await mongoose.connect('mongodb://localhost:27017/twitter_like')
+    await mongoose.connect(process.env.MONGO_URI!)
 }
